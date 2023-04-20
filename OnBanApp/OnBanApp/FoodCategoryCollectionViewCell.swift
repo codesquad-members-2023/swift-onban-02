2630bf4514dbcfd23bc012f2a5cdfe012f73c2f9
SONG <geun2121@gmail.com>
2023-04-20 17:43:07 +0900

    FoodCategoryCollectionViewCell 추가
    - Cell 파일 생성

---
+++ OnBanApp/OnBanApp/FoodCategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FoodCategoryCollectionViewCell.swift
+//  OnBanApp
+//
+//  Created by SONG on 2023/04/19.
+//
+
+import UIKit
+
+class FoodCategoryCollectionViewCell: UICollectionViewCell {
+    
+}
