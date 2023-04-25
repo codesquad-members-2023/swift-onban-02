64a10cb3ca433cba3d7882e9d087cdd7ca17120f
SONG <geun2121@gmail.com>
2023-04-25 11:43:40 +0900

    Model 파일 생성

---
+++ OnBanApp/OnBanApp/Model/Photo.swift
@@ -0,0 +1,12 @@
+//
+//  Photo.swift
+//  OnBanApp
+//
+//  Created by SONG on 2023/04/25.
+//
+
+import Foundation
+
+struct Photo {
+    
+}
---
+++ OnBanApp/OnBanApp/Model/Price.swift
@@ -0,0 +1,12 @@
+//
+//  Price.swift
+//  OnBanApp
+//
+//  Created by SONG on 2023/04/25.
+//
+
+import Foundation
+
+struct Price {
+    
+}
