b7f86942b7b5ca4d2812417d9845f88e6133a73c
SONG <geun2121@gmail.com>
2023-04-27 14:21:00 +0900

    Lint rule에 부합하게 수정 및 image파일 추가

--- OnBanApp/OnBanApp/HeaderData.swift
+++ OnBanApp/OnBanApp/HeaderData.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 struct HeaderData {
-    private(set) var title : [String] = ["온 가족이 좋아하는 든든한 메인 요리","정성과 건강이 가득 담긴 국물 요리","식탁을 풍성하게 하는 정갈한 밑반찬"]
+    private(set) var title: [String] = ["온 가족이 좋아하는 든든한 메인 요리", "정성과 건강이 가득 담긴 국물 요리", "식탁을 풍성하게 하는 정갈한 밑반찬"]
 }
-
 
