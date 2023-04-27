b69d3ef845cded5bb42c52467ee1178e87599b62
SONG <geun2121@gmail.com>
2023-04-27 14:24:52 +0900

    Title UILabel 선언
    - 화면 크기 비율에 맞게 font사이즈 설정 
    - 일단 "안녕하세요" 고정값

--- OnBanApp/OnBanApp/Views/FoodCardViewComponents.swift
+++ OnBanApp/OnBanApp/Views/FoodCardViewComponents.swift
@@ -11,4 +11,13 @@ struct FoodCardViewComponents {
     private(set) var photo: UIImageView = {
         return UIImageView(image: UIImage(named: "abcd.jpeg"))
     }()
+    
+    private(set) var title: UILabel = {
+        let title = UILabel()
+        title.textRect(forBounds: CGRect(), limitedToNumberOfLines: 1)
+        title.text = "안녕하세요"
+        title.textAlignment = .left
+        title.font = UIFont.systemFont(ofSize: UIScreen.main.bounds.height*(8/375), weight: .bold)
+        return title
+    }()
 }
