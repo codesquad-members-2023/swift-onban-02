ffe76c610b2d2c31be798b6ba5b7050db5a395f4
SONG <geun2121@gmail.com>
2023-05-06 18:09:59 +0900

    PriceView 추가
    - FoodCardViewComponents 제거를 위해서 분리

---
+++ OnBanApp/OnBanApp/Views/SPriceView.swift
@@ -0,0 +1,42 @@
+//
+//  PriceView.swift
+//  OnBanApp
+//
+//  Created by SONG on 2023/05/06.
+//
+
+import UIKit
+
+class SPriceView: UILabel {
+    //무조건 있는놈
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        self.text = "12,640원"
+        self.font = UIFont.systemFont(ofSize: UIScreen.main.bounds.height*(8/375), weight: .bold)
+        self.textColor = .black
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        guard let superview = self.superview else {
+            return
+        }
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.5),
+            self.heightAnchor.constraint(equalTo: superview.heightAnchor)
+        ])
+    }
+
+}
